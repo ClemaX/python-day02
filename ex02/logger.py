60eb2ec014d473c900423e8fe2bb67c97bd1d197
Clément Hamada <clementhamada@protonmail.com>
2020-05-29 20:28:03 +0200

    ex02: Formatting

--- ex02/logger.py
+++ ex02/logger.py
@@ -19,7 +19,7 @@ def log(method):
             elapsed *= 1000
             time_unit = 'ms'
         logfile.write(f"({username})Running: {name:<18} ")
-        logfile.write(f"[ exec-time = {elapsed:.3f} {time_unit:<2}]\n")
+        logfile.write(f"[ exec-time = {elapsed:.3f} {time_unit:<2} ]\n")
         logfile.close()
         return result
     return logged
